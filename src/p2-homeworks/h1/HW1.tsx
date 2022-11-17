0a47891a9271489db0af13c918fd4192e9ef3372
Mass00 <krivmann00@gmail.com>
2022-11-17 15:52:18 +0200

    udate

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,27 +1,62 @@
 import React from 'react'
+import st from './HW1.module.css'
+import Message from "./Message";
 
-const messageData = {
-    avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
-}
+const messageData = [
+    {
+        avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+        name: 'Ivan',
+        message: 'Hello, she didn’t do anything and rested all day, how are you?',
+        time: '22:00',
+    },
+    {
+        avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
+        name: 'Bolvan',
+        message: 'Hello, how are you, what did you do yesterday?',
+        time: '22:00',
+    },
+
+]
 
 function HW1() {
     return (
         <div>
             <hr/>
-            homeworks 1
+            <div className={st.container}>
+                <h1>Hometask #1</h1>
 
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
+                {/*should work (должно работать)*/}
+                <div style={{display: 'flex', flexDirection: "column"}}>
+                    {messageData.map((mes, index) => <Message
+                        key={index}
+                        index={index}
+                        avatar={mes.avatar}
+                        name={mes.name}
+                        message={mes.message}
+                        time={mes.time}
+                    />)}
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'space-between'
+                    }}
+                >
+                    <input
+                        style={{
+                            width: '85%',
+                            outline: 'none',
+                            height: '40px',
+                        }}
+                        type={"text"}
+                    />
+                    <button
+                        style={{
+                            width: '13%'
+                        }}
+                    >Send</button>
+                </div>
+            </div>
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeMessage/>*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import st from './Message.module.css'
 
-function Message() {
+type MessagePropsTypes = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+    index: number
+}
+
+function Message(
+    {
+        avatar, name, message, time, index
+    }: MessagePropsTypes
+) {
+
+    const rootMsgWrapper = index % 2 ? st.msgWrapper : [st.msgWrapper, st.active].join(" ")
+    const rootBody = index % 2 ?  [st.body, st.active].join(" ") : st.body
     return (
-        <div>
-
+        <div className={rootMsgWrapper}>
+            <div className={st.message}>
+                <div className={rootBody}>
+                    <h3>{name}</h3>
+                    <span>{message}</span>
+                </div>
+                <div
+                    className={st.wrapper}
+                    style={{order: index % 2 ? -1: 1}}
+                >
+                    <div className={st.avatar}>
+                        <img src={avatar}/>
+                    </div>
+                    <div className={st.time}>
+                        <span>{time}</span>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
